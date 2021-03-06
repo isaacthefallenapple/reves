07385e9a8423096e895a8efd7a0accae7ff22c8d
isaacthefallenapple <isaacthefallenapple@gmail.com>
2021-03-06 13:54:34 +0100

    Provide codec methods and update function

--- src/TypedDict.elm
+++ src/TypedDict.elm
@@ -1,8 +1,26 @@
-module TypedDict exposing (TypedDict, from, fromListWithDefault, get, set, unwrap)
+module TypedDict exposing (TypedDict, decoder, encode, from, fromListWithDefault, get, set, unwrap, update)
+
+import Json.Decode as Decode
+import Json.Encode as Encode
 
 
 type TypedDict k v
     = TypedDict (List ( k, v ))
+
+
+encode : (k -> String) -> (v -> Encode.Value) -> TypedDict k v -> Encode.Value
+encode toString valueEncoder (TypedDict dict) =
+    Encode.object (List.map (Tuple.mapBoth toString valueEncoder) dict)
+
+
+decoder : (String -> k) -> Decode.Decoder v -> Decode.Decoder (TypedDict k v)
+decoder fromString valueDecoder =
+    Decode.map
+        (List.map
+            (Tuple.mapFirst fromString)
+            >> from
+        )
+        (Decode.keyValuePairs valueDecoder)
 
 
 from : List ( k, v ) -> TypedDict k v
@@ -34,6 +52,20 @@ set key val (TypedDict dict) =
         (\( k, v ) ->
             if k == key then
                 ( k, val )
+
+            else
+                ( k, v )
+        )
+        dict
+        |> TypedDict
+
+
+update : k -> (v -> v) -> TypedDict k v -> TypedDict k v
+update key updater (TypedDict dict) =
+    List.map
+        (\( k, v ) ->
+            if k == key then
+                ( k, updater v )
 
             else
                 ( k, v )
